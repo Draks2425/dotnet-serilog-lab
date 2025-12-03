ee2e6cea02c483245cff6736058ce7d4a713210d
Kacper Kraus <kacperkraus2425@gmail.com>
2025-12-03 10:37:40 +0100

    Grammar enchanted

--- SerilogDiDemo/SerilogDiDemo/Program.cs
+++ SerilogDiDemo/SerilogDiDemo/Program.cs
@@ -4,27 +4,24 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Serilog;
 
-// 1. Setup Builder - this builds our application
 var builder = Host.CreateApplicationBuilder(args);
 
-// 2. Serilog Configuration - load settings from appsettings.json
 builder.Services.AddSerilog(config => {
     config.ReadFrom.Configuration(builder.Configuration);
 });
 
 builder.Services.AddTransient<MyService>();
-// 3. Build the host (the DI container is now ready)
+//build the host the DI container
 using IHost host = builder.Build();
 
 Console.WriteLine("--- TASK 3: Manual Logging ---");
 
-// 4. Create a "Scope" to retrieve the logger manually
 using (var scope = host.Services.CreateScope())
 {
-    // Request the logger from the container
+    //request the logger from the container
     var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
 
-    // 5. Log various levels (Task 3)
+    //3
     logger.LogInformation("This is info - everything is working.");
     logger.LogWarning("This is warning - attention!");
     logger.LogError("This is error - something exploded (simulated).");
